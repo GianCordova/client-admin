1b4e9025863802c56876a735fc897ec9917c0f6f
GianCordova <gcordova-2024016@kinal.edu.gt>
2026-04-20 08:15:14 -0600

    Se completaron las consultas

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -6,6 +6,8 @@ import { BrowserRouter } from 'react-router-dom';
 import './../styles/index.css'
 import App from './App.jsx'
 
+// console.log("Comprobando variables", import.meta.env.VITE_API_URL);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
